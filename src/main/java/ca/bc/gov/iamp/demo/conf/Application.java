55ce0b85b496d72c00373f184eb88e144c2859dc
Samir Saad <samir.saad@gov.bc.ca>
2019-03-24 11:54:28 -0400

    remove hystrix

--- src/main/java/ca/bc/gov/iamp/demo/conf/Application.java
+++ src/main/java/ca/bc/gov/iamp/demo/conf/Application.java
@@ -2,11 +2,9 @@ package ca.bc.gov.iamp.demo.conf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 @ComponentScan(basePackages = "ca.bc.gov.iamp")
 public class Application {
 	public static void main(String[] args) {
